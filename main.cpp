d478899a2b55a5805649b61c343f8374e235376f
Dmitry Kushpiak[gitKracken] <kudim@mail.ru>
2023-02-12 15:26:14 +0300

    description

--- main.cpp
+++ main.cpp
@@ -2,9 +2,10 @@
 
 int main()
 {
+    // проверка кодеров и декодеров поотдельности
     coder();
     decodeblock();
-    // тестирование как в message_handler_libs
+    // тестирование как в message_handler_libs : проверка нарезки и всего в сборе в чсати декодера
     decodereal();
 
     return EXIT_SUCCESS;
